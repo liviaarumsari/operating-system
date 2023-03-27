b5612e4ebb73d4c8dd1716e9b6294ce367a89f2a
Rinaldy Adin <16521390@mahasiswa.itb.ac.id>
2023-03-28 00:13:54 +0700

    add: init_director_table implementation

--- src/filesystem/fat32.c
+++ src/filesystem/fat32.c
@@ -11,3 +11,18 @@ const uint8_t fs_signature[BLOCK_SIZE] = {
     [BLOCK_SIZE-2] = 'O',
     [BLOCK_SIZE-1] = 'k',
 };
+
+uint32_t cluster_to_lba(uint32_t cluster) {
+    return CLUSTER_BLOCK_COUNT * cluster;
+}
+
+void init_directory_table(struct FAT32DirectoryTable *dir_table, char *name, uint32_t parent_dir_cluster) {
+    for (size_t i = 0; i < 8; i++) {
+        dir_table->table[0].name[i] = name[i];
+    }
+    dir_table->table[0].user_attribute = UATTR_NOT_EMPTY;
+    dir_table->table[0].attribute = ATTR_SUBDIRECTORY;
+    dir_table->table[0].cluster_high = (parent_dir_cluster >> 16) & 0xFFFF;
+    dir_table->table[0].cluster_low = parent_dir_cluster & 0xFFFF;
+    dir_table->table[0].filesize = 0;
+}
