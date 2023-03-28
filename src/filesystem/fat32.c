e5e97d2c500f43d25f40760a1a4efb0a929b3590
Rinaldy Adin <16521390@mahasiswa.itb.ac.id>
2023-03-28 10:54:01 +0700

    add: filesystem initialization

--- src/filesystem/fat32.c
+++ src/filesystem/fat32.c
@@ -12,6 +12,8 @@ const uint8_t fs_signature[BLOCK_SIZE] = {
     [BLOCK_SIZE-1] = 'k',
 };
 
+struct FAT32DriverState driver_state;
+
 uint32_t cluster_to_lba(uint32_t cluster) {
     return CLUSTER_BLOCK_COUNT * cluster;
 }
@@ -26,3 +28,43 @@ void init_directory_table(struct FAT32DirectoryTable *dir_table, char *name, uin
     dir_table->table[0].cluster_low = parent_dir_cluster & 0xFFFF;
     dir_table->table[0].filesize = 0;
 }
+
+bool is_empty_storage() {
+    return !memcmp(BOOT_SECTOR, fs_signature, BLOCK_SIZE);
+}
+
+void create_fat32() {
+    memcpy(BOOT_SECTOR, fs_signature, BLOCK_SIZE);
+    
+    struct FAT32FileAllocationTable fat_table = {
+        .cluster_map = {
+            CLUSTER_0_VALUE,
+            CLUSTER_1_VALUE,
+            FAT32_FAT_END_OF_FILE,
+            [3 ... CLUSTER_MAP_SIZE - 1] = FAT32_FAT_EMPTY_ENTRY
+        }
+    };
+
+    struct FAT32DirectoryTable root_directory;
+    init_directory_table(&root_directory, "root", FAT_CLUSTER_NUMBER);
+
+    write_clusters(&fat_table, FAT_CLUSTER_NUMBER, 1);
+    write_clusters(&root_directory, ROOT_CLUSTER_NUMBER, 1);
+}
+
+void initialize_filesystem_fat32() {
+    if (is_empty_storage()) {
+        create_fat32();
+    } else {
+        read_clusters(&driver_state.fat_table, FAT_CLUSTER_NUMBER, 1);
+    }
+}
+
+void write_clusters(const void *ptr, uint32_t cluster_number, uint8_t cluster_count) {
+    write_blocks(ptr, cluster_to_lba(cluster_number), cluster_count * CLUSTER_BLOCK_COUNT);
+}
+
+void read_clusters(void *ptr, uint32_t cluster_number, uint8_t cluster_count) {
+    read_blocks(ptr, cluster_to_lba(cluster_number), cluster_count * CLUSTER_BLOCK_COUNT);
+}
+
