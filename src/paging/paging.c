22752d789558dec07f20f5b764cdf06252df090f
alifioditya <alifioditya@gmail.com>
2023-04-23 21:43:10 +0700

    feat: added allocate single user page frame implementation

--- src/paging/paging.c
+++ src/paging/paging.c
@@ -33,8 +33,23 @@ int8_t allocate_single_user_page_frame(void *virtual_addr) {
     // Using default QEMU config (128 MiB max memory)
     uint32_t last_physical_addr = (uint32_t) page_driver_state.last_available_physical_addr;
 
-    // TODO : Allocate Page Directory Entry with user privilege
-    return -1;
+    // If physical_addr is out of range (128 MiB or more), return failure
+    if (last_physical_addr >= 0x8000000) {
+        return -1;
+    }
+
+    // Update page directory entry
+    struct PageDirectoryEntryFlag flag = {
+        .present_bit = 1,
+        .write_bit = 1,
+        .user_supervisor = 1
+    };
+    update_page_directory_entry(last_physical_addr, virtual_addr, flag);
+
+    // Update the state of the last available physical address
+    page_driver_state.last_available_physical_addr += PAGE_FRAME_SIZE;
+    
+    return 0;
 }
 
 void flush_single_tlb(void *virtual_addr) {
