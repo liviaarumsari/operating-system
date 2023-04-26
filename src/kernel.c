0afb175778f29ebd4e25a576ae2ea852bd37b2f2
liviaarumsari <13521094@mahasiswa.itb.ac.id>
2023-04-26 23:19:15 +0700

    fix: kernel

--- src/kernel.c
+++ src/kernel.c
@@ -11,29 +11,32 @@
 #include "../include/paging.h"
 
 void kernel_setup(void) {
+    enter_protected_mode(&_gdt_gdtr);
     pic_remap();
     initialize_idt();
     activate_keyboard_interrupt();
     framebuffer_clear();
     framebuffer_set_cursor(0, 0);
     initialize_filesystem_fat32();
-    keyboard_state_activate();
+    gdt_install_tss();
+    set_tss_register();
 
-    struct ClusterBuffer cbuf[5];
-    for (uint32_t i = 0; i < 5; i++)
-        for (uint32_t j = 0; j < CLUSTER_SIZE; j++)
-            cbuf[i].buf[j] = i + 'a';
+    // Allocate first 4 MiB virtual memory
+    allocate_single_user_page_frame((uint8_t*) 0);
 
+    // Write shell into memory (assuming shell is less than 1 MiB)
     struct FAT32DriverRequest request = {
-        .buf                   = cbuf,
-        .name                  = "ikanaide",
+        .buf                   = (uint8_t*) 0,
+        .name                  = "shell",
         .ext                   = "\0\0\0",
         .parent_cluster_number = ROOT_CLUSTER_NUMBER,
-        .buffer_size           = 0,
-    } ;
+        .buffer_size           = 0x100000,
+    };
+    read(request);
 
-    request.buffer_size = CLUSTER_SIZE;
-    write(request);  // Create fragmented file "daijoubu"
+    // Set TSS $esp pointer and jump into shell 
+    set_tss_kernel_current_stack();
+    kernel_execute_user_program((uint8_t *) 0);
 
     while (TRUE);
 }
