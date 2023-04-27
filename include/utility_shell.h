703256dae7ea930ecbb9bb456d8a194e477da644
liviaarumsari <13521094@mahasiswa.itb.ac.id>
2023-04-28 00:42:53 +0700

    feat: command parser shell, string func utils
    Co-authored-by: Rinaldy-Adin <Rinaldy-Adin@users.noreply.github.com>

--- include/utility_shell.h
+++ include/utility_shell.h
@@ -1,11 +1,19 @@
 #ifndef UTILITY_SHELL_H
 #define UTILITY_SHELL_H
 
+#include "../lib/lib-header/stdtype.h"
+
 #define BIOS_BLUE 0x01
 #define BIOS_GREEN 0x02
 #define BIOS_CYAN 0x03
 #define BIOS_GRAY 0x07
 
 extern char* current_directory;
+
+void syscall(uint32_t eax, uint32_t ebx, uint32_t ecx, uint32_t edx);
+
+void puts(char* buf, uint8_t color);
+
+void executeCommand(char* buf);
 
 #endif
--- src/user-shell.c
+++ src/user-shell.c
@@ -1,31 +1,16 @@
 #include "../lib/lib-header/stdtype.h"
-#include "../lib/lib-header/string.h"
 #include "../include/fat32.h"
 #include "../include/utility_shell.h"
 
-void syscall(uint32_t eax, uint32_t ebx, uint32_t ecx, uint32_t edx) {
-    __asm__ volatile("mov %0, %%ebx" : /* <Empty> */ : "r"(ebx));
-    __asm__ volatile("mov %0, %%ecx" : /* <Empty> */ : "r"(ecx));
-    __asm__ volatile("mov %0, %%edx" : /* <Empty> */ : "r"(edx));
-    __asm__ volatile("mov %0, %%eax" : /* <Empty> */ : "r"(eax));
-    // Note : gcc usually use %eax as intermediate register,
-    //        so it need to be the last one to mov
-    __asm__ volatile("int $0x30");
-}
-
-void puts(char* buf, uint8_t color) {
-    syscall(5, (uint32_t) buf, strlen(buf), color);
-}
-
 int main(void) {
-    char buf[16];
+    char buf[256];
     while (TRUE) {
         puts("sOS@OS-IF2230", BIOS_GREEN);
         puts(":", BIOS_GRAY);
         puts(current_directory, BIOS_BLUE);
         puts("$", BIOS_GRAY);
         syscall(4, (uint32_t) buf, 16, 0);
+        executeCommand(buf);
     }
-
     return 0;
 }
